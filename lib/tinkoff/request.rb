a0f3c469827930cf3950e5bb845b81829348ed62
Ivan Fefilov <ivan@coggno.com>
2021-12-26 19:28:13 +0400

    change base_url to test

--- lib/tinkoff/request.rb
+++ lib/tinkoff/request.rb
@@ -1,6 +1,7 @@
 module Tinkoff
   class Request
-    BASE_URL = 'https://securepay.tinkoff.ru/v2/'
+    #BASE_URL = 'https://securepay.tinkoff.ru/v2/'
+    BASE_URL = 'https://rest-api-test.tinkoff.ru/v2/'
 
     def initialize(path, params = {})
       @url = BASE_URL + path
